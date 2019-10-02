0321b8d8377e8c8166bfcab576ad282523cd1c90
David <davidrs.souza@gmail.com>
2019-10-02 08:29:23 -0300

    alteracoes finais

--- renderiza-cart.js
+++ renderiza-cart.js
@@ -4,7 +4,11 @@ xhr.open("GET","products.json");
 xhr.addEventListener("load",function(){
     var resposta = xhr.responseText;
     resposta = JSON.parse(resposta)
-
+    console.log(resposta);
+    console.log(resposta.cart.item[0].image);
+    console.log(resposta.cart.item[0].name);
+    console.log(resposta.cart.item[0].bestPriceFormated);
+    console.log(resposta.cart.item[0].quantity);
     //variáveis
     var total = 0;
     var nome = "nome-prod";
